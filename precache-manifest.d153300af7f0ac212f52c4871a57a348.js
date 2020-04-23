d4f958f20f7a82c6fbaf783600855be32b08984d
Shukan Shah <shukan@Shukans-MBP.home>
2020-04-23 02:51:21 -0400

    Updates

---
+++ precache-manifest.d153300af7f0ac212f52c4871a57a348.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5474fe23a3cd6e7b189ae1c0a420cd",
+    "url": "/VerifAI/index.html"
+  },
+  {
+    "revision": "9edc2bd5658a3912980b",
+    "url": "/VerifAI/static/css/main.5e4b93e1.chunk.css"
+  },
+  {
+    "revision": "80f7f661f563a87e3ab5",
+    "url": "/VerifAI/static/js/2.9488ffcf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/VerifAI/static/js/2.9488ffcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9edc2bd5658a3912980b",
+    "url": "/VerifAI/static/js/main.ba6d5c84.chunk.js"
+  },
+  {
+    "revision": "c914f96d930e2c31e207",
+    "url": "/VerifAI/static/js/runtime-main.61a35488.js"
+  },
+  {
+    "revision": "c4b4e3a29886cd0e75fc98276b906426",
+    "url": "/VerifAI/static/media/fake.c4b4e3a2.jpg"
+  },
+  {
+    "revision": "69c23f19705e39374c15b1ed17684867",
+    "url": "/VerifAI/static/media/real.69c23f19.jpg"
+  },
+  {
+    "revision": "d9802e53faf53fc8267ef6aa21b97b84",
+    "url": "/VerifAI/static/media/splash.d9802e53.jpg"
+  },
+  {
+    "revision": "6e7b16b73709d38c4b4c26bc70afc61b",
+    "url": "/VerifAI/static/media/unsure.6e7b16b7.jpg"
+  }
+]);
